34d6b1a7b22d530ab4e24b9c234352e74db27b07
ETHLias H01 <152612382+ETHLias@users.noreply.github.com>
2026-06-25 19:57:13 +0200

    Starting point of rework

--- control_strategy.py
+++ control_strategy.py
@@ -1,50 +1,64 @@
-cooling_on = 17.0          # cooling on above this
-cooling_off = 15.0         # cooling off below this
-margin = 2.0    # T_ambient < T_room - margin
 
-# compressor timing timesteps
-idle_time = 2   
-run_time = 1      
+
+cooling_on  = 13.0   # °C, start cooling above this
+cooling_off = 9.0   # °C, stop cooling below this
+margin      = 2.0    # °C, ventilation only worthwhile if T_ambient < T_room - margin
+
+idle_time = 600  # s, minimum compressor standstill before a restart
+run_time  = 300  # s, minimum compressor runtime once started
 
 
 class Controller:
-    def __init__(self):
+    def __init__(self, dt: float, m_dot: float, cp: float):
+ 
+        self._dt    = dt
+        self._m_dot = m_dot
+        self._cp    = cp
+
+        self._idle_steps = idle_time / dt
+        self._run_steps  = run_time / dt
+
         self.mode = 0
-        self._comp_off_steps = idle_time 
-        self._comp_on_steps = 0
+        self._comp_off_steps = self._idle_steps   # start ready to run
+        self._comp_on_steps  = 0
 
-    def step(self, T_room: float, T_ambient: float) -> int:
-        vent_on = T_ambient < T_room - margin
-        comp_allowed = self._comp_off_steps >= idle_time
-        must_keep_running = self.mode == 2 and self._comp_on_steps < run_time
+    def step(self, T_room: float, T_ambient: float, Q_server: float) -> int:
+        T_vent_floor = T_ambient + Q_server / (self._m_dot * self._cp)
 
-        if self.mode == 0:
+        vent_useful     = T_ambient < T_room - margin    
+        vent_sufficient = T_vent_floor < cooling_on     
+        use_vent        = vent_useful and vent_sufficient
+
+        comp_allowed      = self._comp_off_steps >= self._idle_steps
+        must_keep_running = self.mode == 2 and self._comp_on_steps < self._run_steps
+
+        if self.mode == 0:                        # off / heating up
             if T_room > cooling_on:
-                if vent_on:
+                if use_vent:
                     self.mode = 1
                 elif comp_allowed:
                     self.mode = 2
 
-        elif self.mode == 1:
+        elif self.mode == 1:                      # ventilating
             if T_room < cooling_off:
                 self.mode = 0
-            elif not vent_on and comp_allowed:
+            elif not use_vent and comp_allowed:  
                 self.mode = 2
 
-        elif self.mode == 2:
+        elif self.mode == 2:                      # AC running
             if must_keep_running:
-                pass 
+                pass
             elif T_room < cooling_off:
                 self.mode = 0
-            elif vent_on:
+            elif use_vent:                        # free cooling became sufficient
                 self.mode = 1
 
         if self.mode == 2:
-            self._comp_on_steps += 1
-            self._comp_off_steps = 0
+            self._comp_on_steps  += 1
+            self._comp_off_steps  = 0
         else:
             self._comp_off_steps += 1
-            self._comp_on_steps = 0
+            self._comp_on_steps   = 0
 
         return self.mode
 
